0b049523d1ed79750fab51e591eb6cb789087cc9
Mannvika <mannyjainlakes@gmail.com>
2025-06-19 16:43:18 +0900

    Created script for visualizing topopgrahic noise and tested trained models.

--- LoadAndDisplayModelResults.py
+++ LoadAndDisplayModelResults.py
@@ -19,9 +19,11 @@ print("Imports successful", flush=True)
 # ===================================================================
 
 MODEL_PATHS = [
-    r"training_run_results_top\best_model_LR_0.0001_WD_1e-07_ED_4_DC_256_128_64_32_LOSS_SmoothL1.pth",
+    r"training_run_results_top\best_model_LR_1e-05_WD_1e-07_ED_4_DC_256_128_64_32_LOSS_SmoothL1.pth",
+    r"training_run_results_top\best_model_LR_1e-05_WD_1e-07_ED_3_DC_256_128_64_LOSS_SmoothL1.pth",
+    r"training_run_results_top\best_model_LR_1e-05_WD_1e-07_ED_3_DC_256_128_64_LOSS_MSELoss.pth"
 ]
-DATA_ROOT = "DataTop"
+DATA_ROOT = r"datasets/TopographicData"
 NUM_SAMPLES = 15
 OUTPUT_DIR = "topo_results"
 
